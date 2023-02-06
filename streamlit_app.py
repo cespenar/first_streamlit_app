231ebb2be72d2752e221967149ee0eedbcd7cced
Jakub Ostrowski <jakub.ostrowski@capgemini.com>
2023-02-06 09:53:43 +0100

    Added selecting fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit
 import pandas
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -7,9 +7,17 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Vegan Egg')
+streamlit.text('Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv(
     'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 )
+my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here, so they can pick the fruit they want to include
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+
+# display the table on the page
 streamlit.dataframe(my_fruit_list)
