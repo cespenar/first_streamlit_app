c55806ac2232ddb1901db6034bc3c1abd12393b2
Jakub Ostrowski <jakub.ostrowski@capgemini.com>
2023-02-07 07:56:44 +0100

    Added snowflake connector.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import requests
+import snowflake.connector
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -39,3 +40,10 @@ fruityvice_response = requests.get(
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+my_cur = my_cnx.cursor()
+my_cur.execute('select current_user(), current_account(), current_region()')
+my_data_row = my_cur.fetchone()
+streamlit.text('Hello from Snowflake:')
+streamlit.text(my_data_row)
